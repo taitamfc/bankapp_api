ab12bfd09371b04cf8516f3a88c7e5c3d139e7d9
tamnguyen <tamnguyen01>
2024-02-21 11:26:30 +0700

    login, register, logout by api

---
+++ app/Http/Requests/LoginAdminRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class LoginAdminRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|max:255',
+            'password' => 'required|max:255',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'required' => 'Trường yêu cầu',
+            'email' => 'Yêu cầu định dạng là Email',
+            'max' => 'Tối đa 255 kí tự'
+        ];
+    }
+
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors(),
+            'success' => false,
+            'has_errors' => true,
+        ], 200));
+    }
+}
