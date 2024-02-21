4ddcf6fff5ebf08ad00e30620dc9e502fb5ab5b8
tamnguyen <tamnguyen01>
2024-02-21 14:21:58 +0700

    feature jo

---
+++ database/migrations/2024_02_21_044901_add_phone_column_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone')->after('email');// Số điện thoại
+            $table->string('password_confirmation')->after('password');// Mật khẩu cấp 2
+            $table->string('referral_code')->after('password_confirmation')->nullable();// Mã giới thiệu
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('phone');
+            $table->dropColumn('password_confirmation');
+            $table->dropColumn('referral_code');
+        });
+    }
+};
