ec75fcfd8d84d4ceb614c56b1b7384cd2bfe900b
hoanglongtpt <hoanglongtpt1999@gmail.com>
2024-03-01 15:59:02 +0700

    sửa xong api App

--- app/Http/Controllers/Api/BankAccountController.php
+++ app/Http/Controllers/Api/BankAccountController.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserBankAccountResource;
 use App\Models\UserBankAccount;
+use Illuminate\Support\Facades\Auth;
+
 class BankAccountController extends Controller
 {
     public function getbankVietqr(Request $request)
     {
-        $items = UserBankAccount::where('user_id', $request->user_id)
+        $items = UserBankAccount::where('user_id', Auth::guard('api')->id())
             ->where('type', $request->type)
             ->first();
         $res = [
