8b1e0d523d8d026448d357467a3268e851a70e95
Nguyễn Tài Tâm <taitam8590@gmail.com>
2024-03-18 14:15:57 +0700

    deploy 18/3-2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,14 @@
     <link rel="icon" href="/favicon.ico" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <meta name="theme-color" content="#000000" />
-    <meta name="description" content="Web site created using create-react-app" />
+    <meta name="description" content="Hệ thống Fake App ngân hàng, Bill ngân hàng chuẩn nhất 2024" />
     <link rel="apple-touch-icon" href="/logo192.png" />
     <link rel="manifest" href="/manifest.json" />
     <link rel="shortcut icon" href="/landing/assets/images/favicon.png" type="image/x-icon" />
     <title>Hệ thống Fake App ngân hàng, Bill ngân hàng chuẩn nhất 2024</title>
     <link href="/css/style.css" rel="stylesheet">
     <link href="/vendor/metismenu/css/metisMenu.min.css" rel="stylesheet">
-    <script defer="defer" src="/static/js/main.78e97ec2.js"></script>
+    <script defer="defer" src="/static/js/main.b02094da.js"></script>
 </head>
 
 <body data-theme="theme_2" data-sidebar-style="full" data-layout="vertical"><noscript>You need to enable JavaScript to
