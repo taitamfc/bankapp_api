b340d08d966c57e947f647bdf91ce861bf919f69
Nguyễn Tài Tâm <taitam8590@gmail.com>
2024-03-23 15:41:54 +0700

    deploy 23-3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,14 @@
     <link rel="icon" href="/images/favicon.png" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <meta name="theme-color" content="#000000" />
-    <meta name="description" content="Hệ thống Fake App ngân hàng, Bill ngân hàng chuẩn nhất 2024" />
+    <meta name="description" content="Web site created using create-react-app" />
     <link rel="apple-touch-icon" href="/logo192.png" />
     <link rel="manifest" href="/manifest.json" />
     <title>Bankokvip</title>
-    <link rel="shortcut icon" href="/landing/assets/images/favicon.png" type="image/x-icon" />
+    <meta name="description" content="Hệ thống Fake App ngân hàng, Bill ngân hàng chuẩn nhất 2024" />
     <link href="/css/style.css" rel="stylesheet">
     <link href="/vendor/metismenu/css/metisMenu.min.css" rel="stylesheet">
-    <script defer="defer" src="/static/js/main.3e610cdd.js"></script>
+    <script defer="defer" src="/static/js/main.eab79c3d.js"></script>
 </head>
 
 <body data-theme="theme_2" data-sidebar-style="full" data-layout="vertical"><noscript>You need to enable JavaScript to
