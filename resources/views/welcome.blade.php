cfb18a76866ddc77ae5c075017ed0c756063945e
Nguyễn Tài Tâm <taitam8590@gmail.com>
2024-04-05 00:43:48 +0700

    deploy 5-4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
     <title>Bankokvip</title>
     <link href="/css/style.css?t=5" rel="stylesheet">
     <link href="/vendor/metismenu/css/metisMenu.min.css" rel="stylesheet">
-    <script defer="defer" src="/static/js/main.ecd78e8d.js"></script>
+    <script defer="defer" src="/static/js/main.56f943b2.js"></script>
 </head>
 
 <body data-theme="theme_2" data-sidebar-style="full" data-layout="vertical"><noscript>You need to enable JavaScript to
