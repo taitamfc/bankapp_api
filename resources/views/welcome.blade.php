dbe45430127301f021a59bd7781ce21924db286b
Nguyễn Tài Tâm <taitam8590@gmail.com>
2024-04-19 20:48:39 +0700

    deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
             font-family: SFProDisplay
         }
     </style>
-    <script defer="defer" src="/static/js/main.ca624f94.js"></script>
+    <script defer="defer" src="/static/js/main.ee3f2259.js"></script>
 </head>
 
 <body data-theme="theme_2" data-sidebar-style="full" data-layout="vertical">
