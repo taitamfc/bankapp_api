fee38820a3c51cf1193d138f57c2bba6c219c785
Nguyễn Tài Tâm <taitam8590@gmail.com>
2024-04-20 23:25:31 +0700

    deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,11 +52,11 @@
             font-family: SFProDisplay
         }
     </style>
-    <script defer="defer" src="/static/js/main.62cd95a2.js"></script>
+    <script defer="defer" src="/static/js/main.a2950525.js"></script>
 </head>
 
 <body data-theme="theme_2" data-sidebar-style="full" data-layout="vertical">
-    <div><span class="load-font font-be font-Bold">font-be</span> <span
+    <div style="position:absolute;top:-100px"><span class="load-font font-be font-Bold">font-be</span> <span
             class="load-font font-be font-Medium">font-be</span> <span
             class="load-font font-be font-Normal">font-be</span> <span
             class="load-font font-be font-Extrabold">font-be</span> <span
