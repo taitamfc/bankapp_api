518eee86ac38f6a610fb85c0bdbb5a65a033138e
taifamfc <taitam8590@gmail.com>
2024-04-24 18:04:33 +0700

    dl 24-4-18-03

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,7 @@
             font-family: SFProDisplay
         }
     </style>
-    <script defer="defer" src="/static/js/main.d59a90f9.js"></script>
+    <script defer="defer" src="/static/js/main.8d5d5231.js"></script>
 </head>
 
 <body data-theme="theme_2" data-sidebar-style="full" data-layout="vertical">
