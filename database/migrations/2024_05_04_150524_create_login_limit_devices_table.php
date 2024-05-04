75a4883e3307899bbc4c0c197401193b993e17e6
hoanglongtpt <hoanglongtpt1999@gmail.com>
2024-05-04 16:39:23 +0700

    add limit device

---
+++ database/migrations/2024_05_04_150524_create_login_limit_devices_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('login_limit_devices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('deviceToken')->nullable();
+            $table->string('browser')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('login_limit_devices');
+    }
+};
