5b03972dde7597f82cf6e8cbcee696c7b17dcc71
Nguyễn Tài Tâm <taitam8590@gmail.com>
2024-05-30 21:27:16 +0700

    dl 30-5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,7 @@
             font-family: SFProDisplay
         }
     </style>
-    <script defer="defer" src="/static/js/main.054ab3fb.js"></script>
+    <script defer="defer" src="/static/js/main.c02f9a84.js"></script>
 </head>
 
 <body data-theme="theme_2" data-sidebar-style="full" data-layout="vertical">
