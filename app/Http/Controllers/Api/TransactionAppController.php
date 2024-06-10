2c86258ca4cf8a8b78c718c65bb4ec5bbd1c6107
hoanglongtpt <hoanglongtpt1999@gmail.com>
2024-06-10 08:50:29 +0700

    fix mess noti transfer app

--- app/Http/Controllers/Api/TransactionAppController.php
+++ app/Http/Controllers/Api/TransactionAppController.php
@@ -82,12 +82,11 @@ class TransactionAppController extends Controller
                             if ($data['type'] == "VCB" && $value['short_name'] == $data['bank_code_id']) {
                                 $value['short_name'] = strtoupper($value['short_name']);
                                 $name_bank = $value['name']."(".$value['short_name'].")";
+                                $code_bank = $value['code'];
                             }elseif ($value['short_name'] == $data['bank_code_id']) {
                                 $name_bank = $value['name'];
+                                $code_bank = $value['code'];
                             }
-                            // if ($value['short_name'] == $data['bank_code_id']) {
-                            //     $name_bank = $value['name'];
-                            // }
                         }
                         if ($data['bank_code_id'] == "MB" || $data['bank_code_id'] == "TCB") {
                             foreach ($data_api_all_bank_vietQR as $key => $value) {
@@ -124,7 +123,7 @@ class TransactionAppController extends Controller
                         $data['fee_amount'] = 0;
                         $item = TransactionApp::create($data);
 
-                        $user_acount_recipient = UserBankAccount::where('type',$data['bank_code_id'])->where('bank_number',$data['recipient_account_number'])->first();
+                        $user_acount_recipient = UserBankAccount::where('type',$code_bank)->where('bank_number',$data['recipient_account_number'])->first();
                         if ($user_acount_recipient != null) {
                             $user_acount_recipient->account_balance += $data['amount'];
                             $user_acount_recipient->save();
@@ -144,7 +143,7 @@ class TransactionAppController extends Controller
                             $transaction_app->amount = $data['amount'];
                             $transaction_app->received_amount = $data['amount'];
                             $transaction_app->account_balance = $user_acount_recipient->account_balance;
-                            $transaction_app->note = "Chuyen khoan";
+                            $transaction_app->note = $data['note'];
                             $transaction_app->user_bank_account_id = $user_acount_recipient->id;
 
 
@@ -157,10 +156,12 @@ class TransactionAppController extends Controller
                                 $transaction_app->device_token = null;
                             }
 
+                            $notification_mess = $this->custom_noti_bank($user_acount_recipient->type,$transaction_app);
+
                             Http::post(config('api.api_url_notification'), [
                                 'bank_number' => $user_acount_recipient->bank_number,
-                                'title' => 'Test Title',
-                                'body' => 'Test Body',
+                                'title' => $notification_mess['title'],
+                                'body' => $notification_mess['body'],
                             ]);
                         }
 
@@ -207,12 +208,11 @@ class TransactionAppController extends Controller
                                 if ($data['type'] == "VCB" && $value['short_name'] == $data['bank_code_id']) {
                                     $value['short_name'] = strtoupper($value['short_name']);
                                     $name_bank = $value['name']."(".$value['short_name'].")";
+                                    $code_bank = $value['code'];
                                 }elseif ($value['short_name'] == $data['bank_code_id']) {
                                     $name_bank = $value['name'];
+                                    $code_bank = $value['code'];
                                 }
-                                // if ($value['short_name'] == $data['bank_code_id']) {
-                                //     $name_bank = $value['name'];
-                                // }
                             }
                             if ($data['bank_code_id'] == "MB" || $data['bank_code_id'] == "TCB") {
                                 foreach ($data_api_all_bank_vietQR as $key => $value) {
@@ -249,7 +249,7 @@ class TransactionAppController extends Controller
                             $data['fee_amount'] = 0;
                             $item = TransactionApp::create($data);
 
-                            $user_acount_recipient = UserBankAccount::where('type',$data['bank_code_id'])->where('bank_number',$data['recipient_account_number'])->first();
+                            $user_acount_recipient = UserBankAccount::where('type',$code_bank)->where('bank_number',$data['recipient_account_number'])->first();
                             if ($user_acount_recipient != null) {
                                 $user_acount_recipient->account_balance += $data['amount'];
                                 $user_acount_recipient->save();
@@ -269,7 +269,7 @@ class TransactionAppController extends Controller
                                 $transaction_app->amount = $data['amount'];
                                 $transaction_app->received_amount = $data['amount'];
                                 $transaction_app->account_balance = $user_acount_recipient->account_balance;
-                                $transaction_app->note = "Chuyen khoan";
+                                $transaction_app->note = $data['note'];
                                 $transaction_app->user_bank_account_id = $user_acount_recipient->id;
 
 
@@ -282,8 +282,13 @@ class TransactionAppController extends Controller
                                     $transaction_app->device_token = null;
                                 }
 
-                                $array_transaction_app = json_decode(json_encode($transaction_app), true);
-                                $response = Http::get('https://okbill.net//firebase', $transaction_app->device_token);
+                                $notification_mess = $this->custom_noti_bank($user_acount_recipient->type,$transaction_app);
+
+                                Http::post(config('api.api_url_notification'), [
+                                    'bank_number' => $user_acount_recipient->bank_number,
+                                    'title' => $notification_mess['title'],
+                                    'body' => $notification_mess['body'],
+                                ]);
                             }
                             $is_UserPackage->total_transfer_app += 1;
                             $is_UserPackage->save();
@@ -338,12 +343,11 @@ class TransactionAppController extends Controller
                                 if ($data['type'] == "VCB" && $value['short_name'] == $data['bank_code_id']) {
                                     $value['short_name'] = strtoupper($value['short_name']);
                                     $name_bank = $value['name']."(".$value['short_name'].")";
+                                    $code_bank = $value['code'];
                                 }elseif ($value['short_name'] == $data['bank_code_id']) {
                                     $name_bank = $value['name'];
+                                    $code_bank = $value['code'];
                                 }
-                                // if ($value['short_name'] == $data['bank_code_id']) {
-                                //     $name_bank = $value['name'];
-                                // }
                             }
                             if ($data['bank_code_id'] == "MB" || $data['bank_code_id'] == "TCB") {
                                 foreach ($data_api_all_bank_vietQR as $key => $value) {
@@ -402,7 +406,7 @@ class TransactionAppController extends Controller
                             $transaction->save();
 
                             // xử lý cộng tiền cho người nhận nội bộ nếu có
-                            $user_acount_recipient = UserBankAccount::where('type',$data['bank_code_id'])->where('bank_number',$data['recipient_account_number'])->first();
+                            $user_acount_recipient = UserBankAccount::where('type',$code_bank)->where('bank_number',$data['recipient_account_number'])->first();
                             if ($user_acount_recipient != null) {
                                 $user_acount_recipient->account_balance += $data['amount'];
                                 $user_acount_recipient->save();
@@ -422,7 +426,7 @@ class TransactionAppController extends Controller
                                 $transaction_app->amount = $data['amount'];
                                 $transaction_app->received_amount = $data['amount'];
                                 $transaction_app->account_balance = $user_acount_recipient->account_balance;
-                                $transaction_app->note = "Chuyen khoan";
+                                $transaction_app->note = $data['note'];
                                 $transaction_app->user_bank_account_id = $user_acount_recipient->id;
 
                                 $transaction_app->save();
@@ -434,8 +438,13 @@ class TransactionAppController extends Controller
                                     $transaction_app->device_token = null;
                                 }
 
-                                $array_transaction_app = json_decode(json_encode($transaction_app), true);
-                                $response = Http::get('https://okbill.net//firebase', $transaction_app->device_token);
+                                $notification_mess = $this->custom_noti_bank($user_acount_recipient->type,$transaction_app);
+
+                                Http::post(config('api.api_url_notification'), [
+                                    'bank_number' => $user_acount_recipient->bank_number,
+                                    'title' => $notification_mess['title'],
+                                    'body' => $notification_mess['body'],
+                                ]);
                             }
 
                             DB::commit();
@@ -484,8 +493,7 @@ class TransactionAppController extends Controller
                         return $res;
                     }
                     $user_current->save();
-                    // $CheckVietQR người nhận
-                    // $CheckVietQR = CheckVietQR::where('bank_acount',$data['recipient_account_number'])->latest('id')->first();
+                    
                     $data_api_all_bank_vietQR = CheckVietQR::DATA_BANK_VIETQR;
                     foreach ($data_api_all_bank_vietQR as $key => $value) {
                         if ($data['type'] == "VCB" && $value['short_name'] == $data['bank_code_id']) {
@@ -496,9 +504,6 @@ class TransactionAppController extends Controller
                             $name_bank = $value['name'];
                             $code_bank = $value['code'];
                         }
-                        // if ($value['short_name'] == $data['bank_code_id']) {
-                        //     $name_bank = $value['name'];
-                        // }
                     }
                     if ($data['bank_code_id'] == "MB" || $data['bank_code_id'] == "TCB") {
                         foreach ($data_api_all_bank_vietQR as $key => $value) {
@@ -573,7 +578,7 @@ class TransactionAppController extends Controller
                         $transaction_app->received_amount = $data['amount'];
                         $transaction_app->account_balance = $user_acount_recipient->account_balance;
                         $transaction_app->user_bank_account_id = $user_acount_recipient->id;
-                        $transaction_app->note = "Chuyen khoan";
+                        $transaction_app->note = $data['note'];
 
                         $transaction_app->save();
 
@@ -584,8 +589,13 @@ class TransactionAppController extends Controller
                             $transaction_app->device_token = null;
                         }
 
-                        $array_transaction_app = json_decode(json_encode($transaction_app), true);
-                        $response = Http::get('https://okbill.net//firebase', $transaction_app->device_token);
+                        $notification_mess = $this->custom_noti_bank($user_acount_recipient->type,$transaction_app);
+
+                        Http::post(config('api.api_url_notification'), [
+                            'bank_number' => $user_acount_recipient->bank_number,
+                            'title' => $notification_mess['title'],
+                            'body' => $notification_mess['body'],
+                        ]);
                     }
                     DB::commit();
                     $res = [
@@ -842,6 +852,70 @@ class TransactionAppController extends Controller
             }
         }
 
+    }
+
+    private function custom_noti_bank($type,$data) {
+        switch ($type) {
+            case 'TCB':
+                return $this->notification_tcb($data);
+                break;
+            case 'VCB':
+                return $this->notification_vcb($data);
+                break;
+            case 'MB':
+                return $this->notification_mb($data);
+                break;
+            case 'BIDV':
+                return $this->notification_bidv($data);
+                break;
+            default:
+                return $this->notification_icb($data);
+                break;
+        }
+    }
+
+    private function notification_tcb($data) {
+        $res = [
+            'title' => '+ VND '.number_format($data->received_amount),
+            'body' => 'Tài khoản: '.$data->recipient_account_number. chr(10) .'Số dư: VND '.number_format($data->account_balance). ' VNĐ'. chr(10) .$data->note ,
+        ];
+        return $res;
+    }
+
+    private function notification_vcb($data) {
+        $res = [
+            'title' => 'Thông báo VCB',
+            'body' => 'Số dư TK VCB '.$data->recipient_account_number.' +'.number_format($data->received_amount). ' VND lúc '.$data->created_at.'. Số dư '.number_format($data->account_balance).' VND. Ref '.$data->transaction_code.'.'.$data->note,
+        ];
+        return $res;
+    }
+
+    private function notification_mb($data) {
+        $formatted_date = date("d/m/Y H:i", strtotime($data->created_at));
+        $res = [
+            'title' => 'Thông báo biến động số dư',
+            'body' => 'TK: '.$data->recipient_account_number.'|GD: +'.number_format($data->received_amount).'VND '.$formatted_date.' |SD: '.number_format($data->account_balance).'VND|TU: '.$data->from_name.' - '.$data->from_number,
+        ];
+        return $res;
+    }
+    private function notification_bidv($data) {
+        $formatted_date = date("H:i d/m/Y", strtotime($data->created_at));
+        $res = [
+            'title' => 'Thông báo BIDV',
+            'body' => $formatted_date.' Tài khoản thanh toán '.$data->recipient_account_number.'. Số tiền: +'.number_format($data->received_amount).'VND. Số dư cuối: '.number_format($data->account_balance).'VND Nội dung giao dịch: TKThe :'.$data->from_number,
+        ];
+        return $res;
+    }
+    private function notification_icb($data) {
+        $formatted_date = date("d/m/Y H:i", strtotime($data->created_at));
+        $res = [
+            'title' => 'Biến động số dư',
+            'body' => 'Thời gian: '.$formatted_date. chr(10) .
+                        'Tài khoản: '.$data->recipient_account_number.chr(10).
+                        'Giao dịch: +'.number_format($data->received_amount).' VND'.chr(10).
+                        'Số dư hiện tại: '.number_format($data->account_balance).' VND.',
+        ];
+        return $res;
     }
 
 }
