2d70faf0e9add5f08709bcfd170f81ec0274313d
Nguyễn Tài Tâm <taitam8590@gmail.com>
2024-06-17 11:11:55 +0700

    dl-17-6-2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&family=Nunito:wght@400;600;700&display=swap"
+        rel="stylesheet">
     <title>Okbill</title>
     <link href="/css/style.css?t=5" rel="stylesheet">
     <link href="/fonts/SF-Pro-Display/SFProDisplay.css" rel="stylesheet">
@@ -19,40 +21,40 @@
     <link href="/fonts/BeVietnamPro/BeVietnamPro.css" rel="stylesheet">
     <link href="/vendor/metismenu/css/metisMenu.min.css" rel="stylesheet">
     <style>
-        .load-font {
-            position: absolute;
-            color: #011d54
-        }
+    .load-font {
+        position: absolute;
+        color: #011d54
+    }
 
-        .font-be {
-            font-family: 'Be Vietnam Pro'
-        }
+    .font-be {
+        font-family: 'Be Vietnam Pro'
+    }
 
-        .font-Normal {
-            font-weight: 400
-        }
+    .font-Normal {
+        font-weight: 400
+    }
 
-        .font-Medium {
-            font-weight: 500
-        }
+    .font-Medium {
+        font-weight: 500
+    }
 
-        .font-Bold {
-            font-weight: 700
-        }
+    .font-Bold {
+        font-weight: 700
+    }
 
-        .font-Extrabold {
-            font-weight: bolder
-        }
+    .font-Extrabold {
+        font-weight: bolder
+    }
 
-        .font-inter {
-            font-family: Inter
-        }
+    .font-inter {
+        font-family: Inter
+    }
 
-        .font-SFProDisplay {
-            font-family: SFProDisplay
-        }
+    .font-SFProDisplay {
+        font-family: SFProDisplay
+    }
     </style>
-    <script defer="defer" src="/static/js/main.bfcb9297.js"></script>
+    <script defer="defer" src="/static/js/main.25ab40b1.js"></script>
 </head>
 
 <body data-theme="theme_2" data-sidebar-style="full" data-layout="vertical">
