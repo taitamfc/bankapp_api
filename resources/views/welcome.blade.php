291e034ddc8bb19343c49018a75f9eeaeca98349
hoanglongtpt <hoanglongtpt1999@gmail.com>
2024-06-25 10:54:11 +0700

    deploy ver 2 long

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,7 +54,7 @@
             font-family: SFProDisplay
         }
     </style>
-    <script defer="defer" src="/static/js/main.0337dc9b.js"></script>
+    <script defer="defer" src="/static/js/main.2e445b26.js"></script>
 </head>
 
 <body data-theme="theme_2" data-sidebar-style="full" data-layout="vertical">
